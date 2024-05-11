ffbc5b85e1a9536784d24f712454a868ffa0e32d
Bryan Cant <bryan.cant@student.kdg.be>
2024-05-11 05:36:47 +0200

    changes now runs on 1.0 - used a different system to patch into - changed default cull time from 0.5 - 2 - tried implementing a different way of adding item to inventory but didnt work I'll wait for someone else to find a better way

--- Settings.cs
+++ Settings.cs
@@ -16,12 +16,12 @@ namespace SpiderKiller
 		internal static void Initialize(ConfigFile config)
 		{
 			ENABLE_CULLING = config.Bind<bool>("Server", "enableCulling", true, "Enable culling of spiders");
-			ENABLE_EXTRA_CULL_REWARD = config.Bind<bool>("Server", "enableExtraCullReward", false, "Enables the extra cull reward of silkworms");
+			ENABLE_EXTRA_CULL_REWARD = config.Bind<bool>("Server", "enableExtraCullReward", false, "(not working for now)Enables the extra cull reward of silkworms");
 			CULL_QUEEN = config.Bind<bool>("Server", "enableQueenCull", true, "Enable culling of Ungora The Spider Queen VBlood boss (this will turn off her aggro and she will die in one hit))");
-			CULL_WAIT_TIME = config.Bind<double>("Server", "cullWaitTime", 0.5, "Time in seconds to wait before culling spiders again");
+			CULL_WAIT_TIME = config.Bind<double>("Server", "cullWaitTime", 2, "Time in seconds to wait before culling spiders again");
 			EXTRA_CULL_REWARD_THRESHOLD = config.Bind<int>("Server", "cullAmountThreshold", 5, "Amount of spiders to cull before rewarding extra silkworm(s)");
 			CULL_RANGE = config.Bind<float>("Server", "cullRange", 50f, "Range to check for spiders to cull (5=1tile)");
-			SILKWORM_GIVE_AMOUNT = config.Bind<int>("Server", "silkwormGiveAmount", 1, "Amount of silkworms to give when culling 'cullAmountThreshold' worth of spiders");
+			SILKWORM_GIVE_AMOUNT = config.Bind<int>("Server", "silkwormGiveAmount", 1, "(not working for now) Amount of silkworms to give when culling 'cullAmountThreshold' worth of spiders");
 		}
 	}
 }
